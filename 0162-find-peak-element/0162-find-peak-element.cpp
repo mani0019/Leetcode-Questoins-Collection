3925539f0e4d967156b0399123837bdbd03e98c1
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-06-27 23:56:08 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.62%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,10 +5,9 @@ public:
         int low = 0, high = n - 1;
 
         while(low< high){
-            int mid = low +(high-low)/2;
-            if(nums[mid]< nums[mid+1]) low = mid+1;
-            else high = mid;
-
+            int mid = low + (high-low)/2;
+            if(nums[mid] < nums[mid+1]) low = mid+1;
+            else high = mid ;
         }
 
         return low;
