e48f3b2a226225e78d48d6f2d0d659cc6904cfa2
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-07-05 12:00:32 +0530

    Time: 53 ms (58.38%), Space: 18.8 MB (38.3%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,12 +4,18 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        for(int i =0 ;i<m;i++){
-            for(int j =0 ;j<n;j++){
-                if(matrix[i][j]==target) return 1;
-            }
+        int row = 0;
+        int col = n-1;
+
+        while(row < m && col >=0){
+            if(matrix[row][col]== target) return true;
+            else if(matrix[row][col] < target) row++;
+            else col--;
 
         }
+        return 0;
+
+
         
         return 0 ;
     }
