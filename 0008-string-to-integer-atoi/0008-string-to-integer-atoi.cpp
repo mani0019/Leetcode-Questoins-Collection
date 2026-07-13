e7d88af62721289db4c30136df23be00035f07c9
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-07-13 07:45:55 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.69%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
+const int INT_MIN_VAL = -2147483648;
+const int INT_MAX_VAL = 2147483647;
+    int helper(const string &s, int i, long long num, int sign) {
+    // If we are out of bounds or at a non-digit, return
+    if (i >= s.size() || !isdigit(s[i]))
+        return (int)(sign * num);
+
+    // Update num with current digit
+    num = num * 10 + (s[i] - '0');
+
+    // Clamp if overflow
+    if (sign * num <= INT_MIN_VAL) return INT_MIN_VAL;
+    if (sign * num >= INT_MAX_VAL) return INT_MAX_VAL;
+
+    // Recurse for next character
+    return helper(s, i + 1, num, sign);
+}
     int myAtoi(string s) {
         int i = 0;
-        int n = s.size();
+        while (i < s.size() && s[i] == ' ') i++;
 
-        // Skip leading spaces
-        while(i < n && s[i] == ' ')
-            i++;
+    // Handle sign
+    int sign = 1;
+    if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+        sign = (s[i] == '-') ? -1 : 1;
+        i++;
+    }
 
-        // Sign
-        int sign = 1;
-        if(i < n && (s[i] == '+' || s[i] == '-')) {
-            if(s[i] == '-')
-                sign = -1;
-            i++;
-        }
+    // Call recursive helper
+    return helper(s, i, 0, sign);
 
-        long long num = 0;
-
-        // Digits
-        while(i < n && isdigit(s[i])) {
-            num = num * 10 + (s[i] - '0');
-
-            // Overflow check
-            if(sign * num > INT_MAX)
-                return INT_MAX;
-
-            if(sign * num < INT_MIN)
-                return INT_MIN;
-
-            i++;
-        }
-
-        return sign * num;
     }
 };
