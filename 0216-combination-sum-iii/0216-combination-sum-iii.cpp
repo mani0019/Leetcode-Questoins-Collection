59d5f564fb9ce3fa20b705c134c4fe9e85dfbc90
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-07-16 14:50:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.42%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void help(int i , int k ,int n , int sumTillNow ,vector<vector<int>>&ans , vector<int>&subset){
+        if(k<0) return;
+        if(k ==0){
+            if(sumTillNow ==n){
+                ans.push_back(subset);
+                return;
+            }
+        }
+        if(i>9) return;
+        
+        sumTillNow+=i;
+        subset.push_back(i);
+        help(i+1,k-1,n,sumTillNow,ans,subset);
+        sumTillNow-=i;
+        subset.pop_back();
+        help(i+1 ,k,n,sumTillNow , ans , subset);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>subset;
+        help(1 ,k ,n ,0,ans,subset);
+        return ans;
+        
+    }
+};
