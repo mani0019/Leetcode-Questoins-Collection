44b31e667dc3039feedb7baf320cb1774d307faf
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-07-16 15:30:32 +0530

    Time: 0 ms (100%), Space: 10.4 MB (67.95%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ void solve(int idx,
+           vector<int>& nums,
+           vector<int>& ds,
+           vector<vector<int>>& ans)
+{
+    ans.push_back(ds);
+
+    for(int i=idx;i<nums.size();i++)
+    {
+        if(i>idx && nums[i]==nums[i-1])
+            continue;
+
+        ds.push_back(nums[i]);
+
+        solve(i+1, nums, ds, ans);
+
+        ds.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>ds;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+       solve(0, nums ,ds ,ans);
+
+    return ans;
+
+        
+
+        
+    }
+};
