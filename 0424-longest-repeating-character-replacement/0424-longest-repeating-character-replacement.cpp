45e0855adfd583c136855ee4141c74f15e562e55
Manish Kurhe <165509685+mani0019@users.noreply.github.com>
2026-08-23 23:28:52 +0530

    Time: 0 ms (100%), Space: 11 MB (42.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.length();
-        int maxF = 0;
-        int maxLen = 0;
-        unordered_map<int,int>mp;
-       int j = 0;
-        for(int i =0;i<n;i++){
-             mp[s[i]]++;
-             maxF = max(maxF , mp[s[i]]);
+        
 
-             if(i - j +1 - maxF > k){
-                mp[s[j]]--;
-                j++;
-             }
-             maxLen = max(maxLen , i - j+1);
-        }
-        return maxLen;
+         int left =0;
+        int mxcnt = 0;
+        int freq[26] = {0};
+        int mxF = 0;
+        for(int i =0;i<s.length();i++){
+            freq[s[i] - 'A']++;
+            mxF = max(mxF , freq[s[i] - 'A'] );
+            int window = i-left+1;
+
+            int valid  = window - mxF;
+            
+            if(valid <= k){
+                mxcnt =max(mxcnt , i-left+1);
+            }
+            if(valid >k){
+                while(valid > k){
+                     freq[s[left]-'A']--;
+                    left++;
+
+                    valid  = i-left+1 - mxF;
+
+                }
+            }
+
+
+
+            
+
+
+
+      }
+      return mxcnt;
+
+        
     }
 };
